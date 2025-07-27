fc01787ca942f95aa3e24064f5c91e32dfdf89e0
suprava satapathy <supravasatapathy0@gmail.com>
2025-07-27 22:07:15 +0530

    started building website

---
+++ suprava-satapathy/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+
+    <div className="Suprava-Satapathy">
+      <h1>Welcome to My Website </h1>
+    </div>
+  );
+}
+
+export default App;
